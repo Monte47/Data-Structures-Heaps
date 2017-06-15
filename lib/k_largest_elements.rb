e34587c26627d7e2932ac51cc85245bf1ee0e6fa
Monte Jiran <montejiran@gmail.com>
2017-06-15 09:42:52 -0700

    add heap sort and k largest elements

--- lib/k_largest_elements.rb
+++ lib/k_largest_elements.rb
@@ -1,5 +1,16 @@
 require_relative 'heap'
 
 def k_largest_elements(array, k)
+  prc = Proc.new { |el1, el2| el2 <=> el1 }
 
+  (1..array.length).each do |i|
+    BinaryMinHeap.heapify_up(array, i - 1, i, &prc)
+  end
+
+  (array.length - 1).downto(0).each do |i|
+    array[0], array[i] = array[i], array[0]
+    BinaryMinHeap.heapify_down(array, 0, i, &prc)
+  end
+
+  array[array.length - k..-1]
 end
