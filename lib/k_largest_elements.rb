60c3ce57a081f2a0240793399b8f7a755f11e066
Monte Jiran <montejiran@gmail.com>
2017-06-15 16:32:25 -0700

    fin commit

--- lib/k_largest_elements.rb
+++ lib/k_largest_elements.rb
@@ -7,6 +7,7 @@ def k_largest_elements(array, k)
     BinaryMinHeap.heapify_up(array, i - 1, i, &prc)
   end
 
+
   (array.length - 1).downto(0).each do |i|
     array[0], array[i] = array[i], array[0]
     BinaryMinHeap.heapify_down(array, 0, i, &prc)
