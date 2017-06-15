1f0b09894e4233d47214e4d61e5adc710612c8e3
Monte Jiran <montejiran@gmail.com>
2017-06-14 21:58:57 -0700

    add push method

--- lib/heap.rb
+++ lib/heap.rb
@@ -12,9 +12,9 @@ class BinaryMinHeap
 
   def extract
     @store[0], @store[-1] = @store[-1], @store[0]
-    extraced = @store.pop
-    @store = self.class.heapify_down(@store, 0, @prc)
-    extraced
+    extracted = @store.pop
+    self.class.heapify_down(@store, 0, count, &@prc)
+    extracted
   end
 
   def peek
@@ -22,6 +22,8 @@ class BinaryMinHeap
   end
 
   def push(val)
+    @store << val
+    self.class.heapify_up(@store, count - 1, count, &@prc)
   end
 
   public
@@ -42,23 +44,42 @@ class BinaryMinHeap
   def self.heapify_down(array, parent_idx, len = array.length, &prc)
     prc ||= Proc.new { |el1, el2| el1 <=> el2 }
 
-    indices = self.child_indices(len, parent_idx)
+    children_indices = self.child_indices(len, parent_idx)
 
-    if indices.length == 0
+    if children_indices.length == 0
       return array
-    elsif indices.length == 1
-      child_idx = indices[0]
+    elsif children_indices.length == 1
+      child_idx = children_indices[0]
     else
-      child_idx = prc.call( array[indices[0]], array[indices[1]] ) == -1 ? indices[0] : indices[1]
+      if prc.call( array[children_indices[0]], array[children_indices[1]] ) == 1
+        child_idx = children_indices[1]
+      else
+        child_idx = children_indices[0]
+      end
     end
 
     if prc.call(array[child_idx], array[parent_idx]) == -1
       array[child_idx], array[parent_idx] = array[parent_idx], array[child_idx]
     end
+
     self.heapify_down(array, child_idx, len, &prc)
     array
+
   end
 
   def self.heapify_up(array, child_idx, len = array.length, &prc)
+    return array if child_idx == 0
+
+    prc ||= Proc.new { |el1, el2| el1 <=> el2 }
+
+    parent_idx = self.parent_index(child_idx)
+
+    if prc.call(array[child_idx],array[parent_idx]) == -1
+      array[child_idx], array[parent_idx] = array[parent_idx], array[child_idx]
+    end
+
+    self.heapify_up(array, parent_idx, len, &prc)
+    array
+
   end
 end
